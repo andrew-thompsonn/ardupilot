9adb4548c0d8475eb179a0dea5ae2f7c8f8e1bd9
samvgarza <svgarza@protonmail.com>
2022-01-16 16:17:26 -0700

    Sam's hello world

--- ArduPlane/RALPHIE/Hello_world/hello_world_functions.cpp
+++ ArduPlane/RALPHIE/Hello_world/hello_world_functions.cpp
@@ -10,3 +10,11 @@ void helloWorldCarson(){
 	// carson's functions for hello world ticket 
 	// cout << "Hellow World!";
 }
+
+void helloWorldSam(){
+	// petition to change all of ardupilot to python
+	//def helloWorldSam():
+		// print("Hello World")
+		//
+}
+
