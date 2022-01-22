5cc9713f6541b492fe90aa4705ba958bc8ef0a2e
qule5588 <qule5588@colorado.edu>
2022-01-22 15:40:35 -0700

    Connection error from the use of the matMult function of ardupilot. Still trying to fix it.

--- ArduPlane/matrixMath.cpp
+++ ArduPlane/matrixMath.cpp
@@ -1,12 +1,14 @@
 #include "matrixMath.h"
 
 void matrixMathFuncs::matrixMathTest(int num){
-    float testMat1[num], testMat2[num];
+    /*float testMat1[num], testMat2[num];
     for(uint8_t i = 0;i<num;i++) {
         testMat1[i] = 6;
         testMat2[i] = 2;
+	printf("testMat1 val: %f\n testMat2 val: %f\n",testMat1[i],testMat2[i]);
     }
     
+    // error seems to be coming from the use of mat_mul
     float outMat[num];
     mat_mul(testMat1, testMat2, outMat, num);
 
@@ -14,6 +16,6 @@ void matrixMathFuncs::matrixMathTest(int num){
         for (int j = 0; j < num; j++)
             printf("%.10f  ", outMat[i * num + j]);
         printf("\n");
-    }
+    }*/	
 
 }
