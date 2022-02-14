8f8eee5b88772caea2a515f6d0d79bb8e1865dec
Sunny Sarkar <sarkarsunny29@gmail.com>
2022-02-14 13:32:16 -0700

    Class unit test update

--- libraries/AP_Common/tests/test_ralphie.cpp
+++ libraries/AP_Common/tests/test_ralphie.cpp
@@ -1,6 +1,7 @@
 #include <AP_gtest.h>
 
 #include <AP_Math/AP_Math.h>
+#include "../../../ArduPlane/RALPHIE_UNIT_TEST.h"
 
 
 TEST(GTEST_RALPHIE, SimpleTest)
@@ -8,5 +9,10 @@ TEST(GTEST_RALPHIE, SimpleTest)
     EXPECT_EQ(1, 1);
 }
 
+TEST(GTEST_RALPHIE, classSimpleTest)
+{
+    RALPHIE RALPHIEUnitTest;
+    EXPECT_EQ(RALPHIE.testUNIT(1,2), 3)
+}
 
 AP_GTEST_MAIN()
